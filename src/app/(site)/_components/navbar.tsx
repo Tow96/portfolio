950c6892a30bee83a66199c1da11e0522083a9e3
Jose Tow <jose.towe@gmail.com>
2023-09-27 17:18:48 -0600

    Added basic loading and not found components

--- src/app/(site)/_components/navbar.tsx
+++ src/app/(site)/_components/navbar.tsx
@@ -1,3 +1,13 @@
-const Navbar = () => <div>NavbarComponent works</div>;
+import Link from 'next/link';
+
+const Navbar = () => (
+  <div className="w-32 flex items-center space-x-10">
+    <Link href="/">Home</Link>
+    <Link href="/about">About</Link>
+    <Link href="/projects">Projects</Link>
+    <Link href="/blog">Blog</Link>
+    <Link href="/contact">Contact</Link>
+  </div>
+);
 
 export default Navbar;
