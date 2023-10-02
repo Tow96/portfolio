c053f794f055b07f5f4be20205184a59ea700aab
Jose Tow <jose.towe@gmail.com>
2023-10-02 15:32:02 -0600

    Added project listing

--- src/app/(site)/projects/page.tsx
+++ src/app/(site)/projects/page.tsx
@@ -1,3 +1,15 @@
-const ProjectsPage = () => <div>PROJECTS</div>;
+import { getProjects } from '@/sanity/utils';
+
+const ProjectsPage = async () => {
+  const projects = await getProjects();
+
+  return (
+    <section>
+      {projects.map(project => (
+        <article key={project._id}>{project.name}</article>
+      ))}
+    </section>
+  );
+};
 
 export default ProjectsPage;
