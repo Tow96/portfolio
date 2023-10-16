bd0c4f29ba26a54ff94f76efb87d34108b1241d9
Jose Tow <jose.towe@gmail.com>
2023-10-16 16:50:46 -0600

    Added base styling to contacts

--- src/app/(site)/_components/contact-footer.tsx
+++ src/app/(site)/_components/contact-footer.tsx
@@ -7,12 +7,14 @@ export const ContactFooter = async () => {
 
   return (
     <footer id="contact">
-      <section className="max-w-4-xl mx-auto p-4 pb-0 ">
+      <section className="max-w-4-xl mx-auto bg-zinc-700 p-4 pb-0 text-zinc-200">
         <address className="not-italic">
-          <h6>Contact</h6>
+          <h6 className="text-2xl font-semibold md:text-lg">Contact</h6>
           <ul>
             {contactLinks.map(link => (
-              <li key={link._id}>
+              <li
+                key={link._id}
+                className="pt-4 text-lg underline underline-offset-4 md:pt-2 md:text-sm">
                 <Link href={link.url} target="_blank" rel="noopener noreferrer">
                   {link.name}
                 </Link>
