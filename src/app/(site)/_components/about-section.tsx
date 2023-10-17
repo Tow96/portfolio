2e68a35141d97c34a9af4a40bcf341fe5de00653
Jose Tow <jose.towe@gmail.com>
2023-10-17 16:12:58 -0600

    Working on about-me css

--- src/app/(site)/_components/about-section.tsx
+++ src/app/(site)/_components/about-section.tsx
@@ -8,26 +8,32 @@ export const AboutSection = async () => {
   }%`;
 
   return (
-    <section>
-      <h2>About Me</h2>
-      <ul>
-        {aboutMe.blurbs.map((blurb, i) => (
-          <li key={i}>{blurb}</li>
-        ))}
-      </ul>
-      {aboutMe.paragraphs.map((paragraph, i) => (
-        <article key={i}>
-          <h3>{paragraph.title}</h3>
-          <p>{paragraph.text}</p>
-        </article>
-      ))}
-      <Image
-        alt={aboutMe.image.alt}
-        src={aboutMe.image.url}
-        width={80}
-        height={80}
-        style={{ objectFit: 'cover', objectPosition: hotspot }}
-      />
+    <section className="section-min-height flex justify-center bg-zinc-300">
+      <div className="max-w-5xl p-6">
+        <h2 className="text-2xl font-semibold">About Me</h2>
+        <ul>
+          {aboutMe.blurbs.map((blurb, i) => (
+            <li key={i} className="text-zinc-700">
+              {blurb}
+            </li>
+          ))}
+        </ul>
+        <div className="relative">
+          {aboutMe.paragraphs.map((paragraph, i) => (
+            <article key={i}>
+              <h3 className="text-lg font-medium underline">{paragraph.title}</h3>
+              <p>{paragraph.text}</p>
+            </article>
+          ))}
+          <Image
+            alt={aboutMe.image.alt}
+            src={aboutMe.image.url}
+            width={80}
+            height={80}
+            style={{ objectFit: 'cover', objectPosition: hotspot }}
+          />
+        </div>
+      </div>
     </section>
   );
 };
