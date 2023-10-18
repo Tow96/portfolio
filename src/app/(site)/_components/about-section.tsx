5a68c21139796d8735069dcdbe43a7eb2cc1b4b1
Jose Tow <jose.towe@gmail.com>
2023-10-17 20:28:56 -0600

    Added CSS for about me page

--- src/app/(site)/_components/about-section.tsx
+++ src/app/(site)/_components/about-section.tsx
@@ -8,30 +8,36 @@ export const AboutSection = async () => {
   }%`;
 
   return (
-    <section className="section-min-height flex justify-center bg-zinc-300">
-      <div className="max-w-5xl p-6">
+    <section className="section-min-height relative flex justify-center bg-zinc-300">
+      <div className="max-w-5xl px-6 py-6 sm:px-12">
         <h2 className="text-2xl font-semibold">About Me</h2>
         <ul>
           {aboutMe.blurbs.map((blurb, i) => (
-            <li key={i} className="text-zinc-700">
+            <li key={i} className="min-h-[48px] text-zinc-700">
               {blurb}
             </li>
           ))}
         </ul>
-        <div className="relative">
-          {aboutMe.paragraphs.map((paragraph, i) => (
-            <article key={i}>
-              <h3 className="text-lg font-medium underline">{paragraph.title}</h3>
-              <p>{paragraph.text}</p>
-            </article>
-          ))}
-          <Image
-            alt={aboutMe.image.alt}
-            src={aboutMe.image.url}
-            width={80}
-            height={80}
-            style={{ objectFit: 'cover', objectPosition: hotspot }}
-          />
+        <div className="flex pt-4">
+          <div className="pr-4 lg:mt-auto lg:pb-12">
+            <div className="absolute right-7 top-6 aspect-square w-24 overflow-hidden rounded-full border-4 border-zinc-100 shadow-inner lg:relative lg:w-48">
+              <Image
+                alt={aboutMe.image.alt}
+                src={aboutMe.image.url}
+                style={{ objectFit: 'cover', objectPosition: hotspot }}
+                fill
+                sizes="(min-width: 1024px) 12rem, 6rem"
+              />
+            </div>
+          </div>
+          <div className="grow">
+            {aboutMe.paragraphs.map((paragraph, i) => (
+              <article key={i} className="pb-4">
+                <h3 className="text-lg font-medium underline">{paragraph.title}</h3>
+                <p>{paragraph.text}</p>
+              </article>
+            ))}
+          </div>
         </div>
       </div>
     </section>
